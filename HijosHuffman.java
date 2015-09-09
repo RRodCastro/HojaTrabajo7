029e31920dbb9f6d33331350b41cf5fdf5271753
RRodCastro <mrcastro9574@gmail.com>
2015-09-08 22:39:39 -0600

    Create HijosHuffman.java

---
+++ HijosHuffman.java
@@ -0,0 +1,36 @@
+
+
+//
+/**
+ * Rodrigo Castro #14092
+ * Hugo Noriega #14097
+ * Algoritmos y Estructuras de Datos
+ * 
+ * 
+ *
+ */
+public class HijosHuffman extends HuffmanArbol {
+	
+	private char caracter;
+	
+
+	public HijosHuffman(int frecuencia, char caracter) {
+		super(frecuencia);
+		// TODO Auto-generated constructor stub
+		caracter = this.caracter;
+	}
+
+
+	public char getCaracter() {
+		return caracter;
+	}
+
+
+	public void setCaracter(char caracter) {
+		this.caracter = caracter;
+	}
+	
+		
+	
+
+}
